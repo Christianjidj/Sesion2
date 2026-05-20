9d8c8193745e60fcc49f4f1679b11db45bcf0d75
Christianjidj <a00573769@tec.mx>
2026-05-20 15:46:07 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "PointerExamples.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    PointerExamples::demoBasicPointer();
+    PointerExamples::demoNullPointer();
+    PointerExamples::demoPointerArithmetic();
+    PointerExamples::demoFunctionPointer();
+    PointerExamples::demoMemberPointer();
+    PointerExamples::demoPolymorphism();
+}
